9baf72951e2275d1fed040b8af228922ac40ba53
ChenYing Kuo <evshary@gmail.com>
2020-11-05 23:27:04 +0800

    Use /home/ros/workspace/zenoh_dds_dep_ws/install/include/.

--- build.rs
+++ build.rs
@@ -8,7 +8,7 @@ fn main() {
     println!("cargo:rustc-link-lib=cdds-util");
     let bindings = bindgen::Builder::default()
         .header("wrapper.h")
-        .clang_arg("-I/home/evshary/workspace/zenoh_dds_dep_ws/install/include/")
+        .clang_arg("-I/home/ros/workspace/zenoh_dds_dep_ws/install/include/")
         .generate_comments(false)
         .generate()
         .expect("Unable to generate bindings");
